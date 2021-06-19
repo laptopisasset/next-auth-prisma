80472e867fdb35792388a1e529edccc2589db989
Soundhar <laptopisasset@gmail.com>
2021-06-19 19:22:12 +0530

    Added prisma and generate.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import { FC } from "react";
 import { AppProps } from "next/app";
+import { Provider } from "next-auth/client";
 
 import "../styles/tailwind.css";
 
 const App: FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <Provider session={pageProps.session}>
+    <Component {...pageProps} />
+  </Provider>
 );
 
 export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,31 @@
 import Head from "next/head";
+import { signIn, signOut, useSession } from "next-auth/client";
 
-const IndexPage = () => (
-  <>
-    <Head>
-      <title>Auth Demo</title>
-    </Head>
-    <nav>
-      <button>Sign In</button>
-      <button>Sign Out</button>
-    </nav>
-  </>
-);
+const IndexPage = () => {
+  const [session] = useSession();
+  return (
+    <>
+      <Head>
+        <title>Auth Demo</title>
+      </Head>
+      <nav>
+        {!session ? (
+          <button onClick={() => signIn("github")}>Github Connect</button>
+        ) : (
+          <>
+            <span>{session.user.name}</span>
+            {session.user.image && (
+              <img
+                src={session.user.image}
+                style={{ width: "25px", borderRadius: "50%" }}
+              />
+            )}
+            <button onClick={() => signOut()}>Sign Out</button>
+          </>
+        )}
+      </nav>
+    </>
+  );
+};
 
 export default IndexPage;
