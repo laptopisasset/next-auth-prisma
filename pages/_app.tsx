4c311537f24c7c96a82f5fa4569344973fcff1dd
Soundhar <laptopisasset@gmail.com>
2021-06-19 16:23:34 +0530

    Added tailwind to project.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react";
 import { AppProps } from "next/app";
 
+import "../styles/tailwind.css";
+
 const App: FC<AppProps> = ({ Component, pageProps }) => (
   <Component {...pageProps} />
 );
