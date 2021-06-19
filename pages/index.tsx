5b642dfee901bed6eed8c7abf1e08437f9f7c105
Soundhar <laptopisasset@gmail.com>
2021-06-19 16:15:31 +0530

    Added basic setup.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,15 @@
-const IndexPage = () => "Hello";
+import Head from "next/head";
+
+const IndexPage = () => (
+  <>
+    <Head>
+      <title>Auth Demo</title>
+    </Head>
+    <nav>
+      <button>Sign In</button>
+      <button>Sign Out</button>
+    </nav>
+  </>
+);
 
 export default IndexPage;
